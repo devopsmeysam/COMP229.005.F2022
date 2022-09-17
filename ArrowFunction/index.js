ca58d7f2668099aa1bfe61364d76d7ae1399fa9b
devopsmeysam <91230909+devopsmeysam@users.noreply.github.com>
2022-09-17 11:50:13 -0400

    Arrow Functions
    Play around with some functions to illustrate the importance of Arrow Function

---
+++ ArrowFunction/index.js
@@ -0,0 +1,88 @@
+var hello;
+
+hello = () => {
+document.getElementById("demo").innerHTML += "Hello 1";
+}
+
+var hello2 = function(){
+    document.getElementById("demo").innerHTML += "Hello 2";
+}
+
+function hello3(){
+    document.getElementById("demo").innerHTML += "Hello 3";
+}
+
+//The window object calls the function:
+window.addEventListener("load", hello);
+
+//A button object calls the function:
+document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("demo").innerHTML += "Hello 1";
+});
+
+document.getElementById("btn2").addEventListener("click", hello2);
+
+var numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(n => n * n);
+// document.write(squares);
+
+var evens = new Array();
+numbers.forEach(n => {
+    if (n % 2 === 0) {
+        evens.push(n);
+    }
+})
+// document.write('<br>');
+// document.write(evens);
+
+// var hello;
+
+// hello = () => {
+// document.getElementById("demo").innerHTML += this;
+// }
+
+// //The window object calls the function:
+// window.addEventListener("load", hello);
+
+// //A button object calls the function:
+// document.getElementById("btn").addEventListener("click", hello);
+
+
+// var numbers = [1,2,3,4,5];
+// const squares = numbers.map(n => n * n);
+// document.write(squares);
+
+// var evens = new Array();
+// numbers.forEach(n => {
+// if (n % 2 === 0) {
+// evens.push(n);
+// }
+// });
+// document.write(evens);
+
+const author = {
+    fullName: "Bob Alice",
+    books: ["Book 01","Book 02","Book 03"],
+    
+    printBooks() {
+    this.books.forEach(book => document.write(book + ' by ' +this.fullName + '<br>'));
+    },
+    
+    printBooks1() {
+    function printValue(book) {
+    document.write(book + ' by ' +this.fullName + '<br>'); // Undefined expected for fullname
+    }
+    
+    this.books.forEach(printValue);
+    },
+    
+    printBooks2() {
+    this.books.forEach(function(book){
+    document.write(book + ' by ' +this.fullName + '<br>'); // Undefined expected for fullname
+    });
+    }
+    };
+    
+    author.printBooks();
+    author.printBooks1();
+    author.printBooks2();
